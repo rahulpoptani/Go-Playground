515cab3336dacd7060ee2071dd288841a34778ea
rahulpoptani <rahulpoptani90@gmail.com>
2024-02-20 09:34:01 +0530

    update

---
+++ cards/deck.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Dimonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Fours"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
